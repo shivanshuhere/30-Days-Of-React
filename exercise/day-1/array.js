ad2081f72281dca6e501a2fb4ac895867364ab0e
Shivanshu <ks7876555@gmail.com>
2024-03-31 18:44:57 +0530

    first commit

---
+++ exercise/day-1/array.js
@@ -0,0 +1,23 @@
+const countries = [
+    "Albania",
+    "Bolivia",
+    "Canada",
+    "Denmark",
+    "Ethiopia",
+    "Finland",
+    "Germany",
+    "Hungary",
+    "Ireland",
+    "Japan",
+    "Kenya",
+];
+
+//1.4
+console.log(
+    "First element : ",
+    countries[0],
+    "\nMiddle element :",
+    countries[Math.floor(countries.length / 2)],
+    "\nLast Element :",
+    countries[countries.length - 1]
+);
