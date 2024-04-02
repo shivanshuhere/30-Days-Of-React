55beb22a64341e04e780660a3fe4d10a296d8afb
Shivanshu <ks7876555@gmail.com>
2024-04-02 17:43:01 +0530

    add frontend and susbcribe elements

--- exercise/day-3/src/main.jsx
+++ exercise/day-3/src/main.jsx
@@ -1,30 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import html from "./images/html_logo.png";
-import css from "./images/css_logo.png";
-import react from "./images/react_logo.png";
-
-const container_style = {
-    display: "flex",
-    justifyContent: "center",
-    height: "100vh",
-    alignItems: "center",
-    gap: "20px",
-    // flexDirection: "column",
-    widht: "100%",
-};
-const img_style = {
-    widht: "100px",
-    height: "100px",
-};
+import FrontendTech from "./FrontendTech";
+import Subscribe from "./Subscribe";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <div style={container_style}>
-            <h1>Frontend Technologies</h1>
-            <img style={img_style} src={html} alt="" />
-            <img style={img_style} src={css} alt="" />
-            <img style={img_style} src={react} alt="" />
-        </div>
+        {FrontendTech}
+        {Subscribe}
     </React.StrictMode>
 );
